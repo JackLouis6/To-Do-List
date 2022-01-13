a0fd6eab5319d38eec3a7768bd252dea443b4108
Jack Louis <jbl5925@gmail.com>
2022-01-13 10:55:24 -0600

    Enabled data persistence

--- To-Do List/ContentView.swift
+++ To-Do List/ContentView.swift
@@ -51,7 +51,8 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 
-struct ToDoItem: Identifiable {
+struct ToDoItem: Identifiable, Codable
+{
     var id = UUID()
     var priority = String()
     var description = String()
